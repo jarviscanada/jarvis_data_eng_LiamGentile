b55885a1f589874a613aba1d669e9c36de67191f
liamgentile <liam.gentile@mail.mcgill.ca>
2021-11-17 17:20:24 +0000

    wrote the host_info script

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
 
-#saving hostname to a variable
-hostname=$(hostname -f)
+# assigning CLI arguments to variables 
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
 
-#saving cpu information to a variable
+# ensuring that the user provides five arguments
+if [ $# -ne 5 ]; then
+      echo 'This script requires five arguments:'
+      echo 'psql_host, psql_port, db_name, psql_user, psql_password'
+      exit 1
+fi
+
+# assigning hardware info commands to variables and pulling out our desired columns
 lscpu_out=`lscpu`
+mem_info_out=$(cat /proc/meminfo)
+
+hostname=$(hostname -f)
 
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 
 cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs) 
-
-#$1-$2="" is telling awk to ignore fields one and two and print $0 says to print the whole line (in this case without one and two)
+ 
 cpu_model=$(echo "$lscpu_out"  | egrep "^Model name:" | awk '{$1=$2=""; print $0}' | xargs)
 
 cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{$1=$2=""; print $0}' | xargs)
-
-#adding +0 to the awk print statement allows me to ignore the K after the number (forced to interpret the 3rd field as a number)
+ 
 l2_cache=$(echo "$lscpu_out"  | egrep "^L2[[:space:]]cache:" | awk '{print $3+0}' | xargs)
 
+total_mem=$(echo "$mem_info_out"  | egrep "^MemTotal:" | awk '{print $2+0}' | xargs)
+
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+insert_stmt="INSERT INTO host_usage(host_name, cpu_number, cpu_architecture, 
+	     cpu_model, cpu_mhz, L2_cache, total_mem, time_stamp) 
+	     VALUES('$host_name', '$cpu_number', '$cpu_architecture', 
+            '$cpu_model', '$cpu_mhz', '$l2_cache', '$total_mem', '$timestamp')"
+
+# exporting password variable for psql child process to inherit
+export POSTGRES_PASSWORD=$psql_password 
+
+# inserting the data into a database using psql, the cli arguments, and the sql insert_stmt
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit 0
